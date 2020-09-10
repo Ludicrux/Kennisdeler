35b21092f31c9828677a13562278b34ab13412d8
Luca Barendse <luca@go2people.nl>
2020-09-10 16:06:01 +0200

    Added basic article model

---
+++ articles/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.conf import settings
+
+from django_extensions.db.models import TimeStampedModel
+
+
+class Tag(TimeStampedModel):
+    name = models.CharField(max_length=50)
+
+class Article(TimeStampedModel):
+    """
+    Model for the user uploaded articles
+    """
+    title = models.CharField(max_length=120)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    short_desc = CharField(max_length=120)
+    long_desc = models.TextField(max_length=1500, null=True, blank=True)
+    image = models.ImageField()
+    uploaded_file = models.FileField(upload_to='')
+    user_likes = models.ManyToManyField(settings.AUTH_USER_MODEL)
+    subject = models.CharField(max_length=25)
+    level = models.CharField(max_length=25)
+    tags = models.ManyToManyField(Tag)
+    file_type = models.CharField(max_length=50)
+    is_public = models.BooleanField()
+    views = models.IntegerField()
+    downloads = models.IntegerField()
