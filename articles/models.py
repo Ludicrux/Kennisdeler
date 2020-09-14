0c4dd9871f946386331e1079ba31006ecd5213fc
Gebruiker12 <gebruiker12@MacBook-Air-van-Gebruiker12.local>
2020-09-14 12:23:07 +0200

    Finished models for article

--- articles/models.py
+++ articles/models.py
@@ -1,30 +1,75 @@
-#
+import uuid
+import os
+
 from django.db import models
 from django.contrib.auth.models import User
 
-#
 from django_extensions.db.models import TimeStampedModel
 
 
+SUBJECT_CHOICES = [
+    ('ASF', 'Asfalt'),
+    ('AST', 'Assistent'),
+    ('BES', 'Bestratingen'),
+    ('BET', 'Betonboren'),
+]
+
+LEVEL_CHOICES = [
+    ('1', 'Niveau 1'),
+    ('2', 'Niveau 2'),
+    ('3', 'Niveau 3'),
+    ('4', 'Niveau 4'),
+]
+
+FILE_TYPE_CHOICES = [
+    ('3DB', '3D-beeld'),
+    ('ANI', 'Animaties'),
+    ('DID', 'Didactische werkvorm'),
+    ('GRO', '(Groeps)opdracht'),
+    ('PRE', 'Presentatie'),
+    ('TOE', 'Toets'),
+    ('NVT', 'N.v.t.'),
+]
+
+def article_file_path(instance, filename):
+    """Generate file path for uploaded file"""
+    ext = filename.split('.')[-1]
+    filename = f'{uuid.uuid4()}.{ext}'
+
+    return os.path.join('files/article-files/', filename) 
+
+def article_image_path(instance, filename):
+    """Generate file path for uploaded image"""
+    ext = filename.split('.')[-1]
+    filename = f'{uuid.uuid4()}.{ext}'
+
+    return os.path.join('images/article-images/', filename) 
+
+
 class Tag(TimeStampedModel):
+    """tags used for article"""
     name = models.CharField(max_length=50)
 
 
 class Article(TimeStampedModel):
-    """
-    Model for the user uploaded articles
-    """
+    """Model for the user uploaded articles"""
     title = models.CharField(max_length=120)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     short_desc = CharField(max_length=120)
-    long_desc = models.TextField(max_length=1500, null=True, blank=True)
-    image = models.ImageField()
-    uploaded_file = models.FileField(upload_to='')
-    user_likes = models.ManyToManyField(User)
-    subject = models.CharField(max_length=25)
-    level = models.CharField(max_length=25)
-    tags = models.ManyToManyField(Tag)
-    file_type = models.CharField(max_length=50)
-    is_public = models.BooleanField()
-    views = models.IntegerField()
-    downloads = models.IntegerField()
+    long_desc = models.TextField(max_length=1500, blank=True)
+    image = models.ImageField(upload_to=article_image_path)
+    uploaded_file = models.FileField(upload_to=article_file_path)
+    user_likes = models.ManyToManyField(User, blank=True)
+    subject = models.CharField(
+        max_length=3, choices=SUBJECT_CHOICES, default='ASF'
+    )
+    level = models.CharField(
+        max_length=1, choices=LEVEL_CHOICES, default='1'
+    )
+    tags = models.ManyToManyField(Tag, blank=True)
+    file_type = models.CharField(
+        max_length=3, choices=FILE_TYPE_CHOICES, default='3DB'
+    )
+    is_public = models.BooleanField(default=False)
+    views = models.IntegerField(default=0)
+    downloads = models.ManyToManyField(User, blank=True)
