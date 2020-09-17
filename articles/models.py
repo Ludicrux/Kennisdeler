45fd218fc9e94f9a880929aacc4a1b4cda4b7ab5
Luca Barendse <luca@go2people.nl>
2020-09-17 09:39:09 +0200

    old local files

--- articles/models.py
+++ articles/models.py
@@ -7,6 +7,9 @@ from django_extensions.db.models import TimeStampedModel
 
 
 class Tag(TimeStampedModel):
+    """
+    tags for the article
+    """
     name = models.CharField(max_length=50)
 
 
@@ -17,14 +20,14 @@ class Article(TimeStampedModel):
     title = models.CharField(max_length=120)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     short_desc = CharField(max_length=120)
-    long_desc = models.TextField(max_length=1500, null=True, blank=True)
-    image = models.ImageField()
-    uploaded_file = models.FileField(upload_to='')
-    user_likes = models.ManyToManyField(User)
+    long_desc = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to='', blank=True)
+    uploaded_file = models.FileField(upload_to='', blank=True)
+    user_likes = models.ManyToManyField(User, related_name='user_likes', help_text)
     subject = models.CharField(max_length=25)
     level = models.CharField(max_length=25)
-    tags = models.ManyToManyField(Tag)
+    tags = models.ManyToManyField(Tag, related_name='tags')
     file_type = models.CharField(max_length=50)
-    is_public = models.BooleanField()
+    is_public = models.BooleanField(default=0)
     views = models.IntegerField()
     downloads = models.IntegerField()
