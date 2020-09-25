df52663a8bbcfd5bea22c309e044ed067417eaa2
Luca Barendse <luca@go2people.nl>
2020-09-25 11:04:37 +0200

    fixed spelling error in user_likes

--- apps/articles/models.py
+++ apps/articles/models.py
@@ -54,7 +54,7 @@ class Article(TimeStampedModel):
     image = models.ImageField(upload_to="images/article-images/")
     uploaded_file = models.FileField(upload_to="documents/article-documents/")
     user_likes = models.ManyToManyField(
-        User, blank=True, through="Likes", related_name="user_likes"
+        User, blank=True, through="Like", related_name="user_likes"
     )
     subject = models.ForeignKey(
         Subject, null=True, on_delete=models.SET_NULL
