d2b94b9522d48b5e4955db06308b946e614f1bcb
Luca Barendse <luca@go2people.nl>
2020-10-20 16:22:01 +0200

    resolved item #2

--- apps/articles/filters.py
+++ apps/articles/filters.py
@@ -3,7 +3,7 @@ Filters for articles
 """
 from django import forms
 from articles import models
-from articles.models import Article
+from articles.models import Article, Subject
 import django_filters
 
 
@@ -11,9 +11,18 @@ class ArticleFilter(django_filters.FilterSet):
     """
     Filter for article list
     """
+    subject = django_filters.ModelChoiceFilter(
+        queryset=Subject.objects.all(),
+        label="Opleiding"
+    )
+    level = django_filters.ChoiceFilter(
+        choices=models.LEVEL_CHOICES,
+        label="Niveau"
+    )
     file_type = django_filters.MultipleChoiceFilter(
         choices=models.FILE_TYPE_CHOICES,
-        widget=forms.CheckboxSelectMultiple
+        widget=forms.CheckboxSelectMultiple,
+        label="Soort document"
     )
 
     class Meta:
